36fd70472d88b1b04b8fdea14f2bacf596bed75c
riengiardi <michalkmiecik9@gmail.com>
2026-05-28 16:56:20 +0200

    0.5 full player movement added

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import circleshape
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(circleshape.CircleShape):
 
@@ -25,6 +25,12 @@ class Player(circleshape.CircleShape):
     def rotate(self, dt: float) -> None:
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt: float) -> None:
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
 
@@ -32,3 +38,7 @@ class Player(circleshape.CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
