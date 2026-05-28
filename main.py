163fd56e87bb6093f47766ade555223a0691d620
riengiardi <michalkmiecik9@gmail.com>
2026-05-28 15:56:00 +0200

    0.1 creating black window and exit logic

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version {pygame.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        # logger function from boot.dev
+        log_state()
+
+        for event in pygame.event.get():
+            # check for quiting by X
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
