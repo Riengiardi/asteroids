bd208fa9bcd458cd3b6425f1672854ddcd4a60b0
riengiardi <michalkmiecik9@gmail.com>
2026-05-29 16:45:46 +0200

    0.9 added gameover screen

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
-# screen resolution
+# screen constants
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+SCREEN_CENTER = (SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
 
 # drawing constants
 LINE_WIDTH = 2
--- main.py
+++ main.py
@@ -7,14 +7,12 @@ from constants import *
 from logger import log_state, log_event
 
 def main():
-    print(f"Starting Asteroids with pygame version {pygame.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0.0
+    gameover = False
+    gameover_font = pygame.font.Font("font.ttf", 64)
 
     drawable, updatable, asteroids, shots = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
     Player.containers = (updatable, drawable)
@@ -33,29 +31,33 @@ def main():
             # check for quiting by X
             if event.type == pygame.QUIT:
                 return
+
+        if not gameover:
+            screen.fill("black")
             
-        screen.fill("black")
-        
-        for d in drawable:
-            d.draw(screen)
-        
-        updatable.update(dt)
+            for d in drawable:
+                d.draw(screen)
+            
+            dt = clock.tick(60) / 1000
+            updatable.update(dt)
 
-        for a in asteroids:
-            if p1.collides_with(a):
-                log_event("player_hit")
-                print("Game over!")
-                sys.exit()
-            for s in shots:
-                if s.collides_with(a):
-                    log_event("asteroid_shot")
-                    a.split()
-                    s.kill()
+            for a in asteroids:
+                if p1.collides_with(a):
+                    log_event("player_hit")
+                    gameover = True
+                    
+                for s in shots:
+                    if s.collides_with(a):
+                        log_event("asteroid_shot")
+                        a.split()
+                        s.kill()
 
-        pygame.display.flip()
-
-        # makes sure that the time between frames is limited to .0166666 (60fps)
-        dt = clock.tick(60) / 1000
+            pygame.display.flip()
+        else:
+            screen.fill("black")
+            gameover_text = gameover_font.render("Game over!", True, "white")
+            screen.blit(gameover_text, gameover_text.get_rect(center=SCREEN_CENTER))
+            pygame.display.flip()
 
 
 
